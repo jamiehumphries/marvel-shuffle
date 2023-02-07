144429fa1c078e28b8e5cf9f92681b6011b5139f
Jamie Humphries <jamie.humphries@softwire.com>
2023-02-07 15:44:24 +0000

    Add card options (always visible for now).

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,13 @@
 const CARD_CHANGE_DELAY_MS = 300;
 
 class Section {
-  constructor(cards) {
-    this.cards = cards;
+  constructor(cardsOrSets) {
+    this.cardsOrSets = cardsOrSets;
+
+    this.cards = cardsOrSets.reduce((cards, cardOrSet) => {
+      return cards.concat(cardOrSet.cards || [cardOrSet]);
+    }, []);
+
     this.typeId = this.cards.reduce((typeId, card) => {
       const cardTypeId = getId(card.constructor);
       if (typeId == null || typeId == cardTypeId) {
@@ -15,6 +20,7 @@ class Section {
     const container = document.getElementById(this.typeId);
     this.elements = {
       button: container.querySelector("button"),
+      options: container.querySelector(".options"),
       name: container.querySelector(".name"),
       slot: container.querySelector(".slot"),
       cardFront: container.querySelector("img.front"),
@@ -23,9 +29,14 @@ class Section {
   }
 
   initialize() {
+    this.cardsOrSets.forEach((cardOrSet) =>
+      cardOrSet.appendOptionsTo(this.elements.options)
+    );
+
     const savedCardId = localStorage.getItem(this.typeId);
     const savedCard = this.cards.find((card) => card.id === savedCardId);
     this.card = savedCard || this.randomCard();
+
     this.elements.button.addEventListener("click", () => this.shuffle(true));
     this.elements.slot.addEventListener("transitionend", (event) =>
       this.onTransitionEnd(event)
@@ -88,15 +99,92 @@ class Section {
   }
 
   randomCard(preventRepeat = false) {
-    const availableCards = preventRepeat
-      ? this.cards.filter((card) => card !== this.card)
-      : this.cards;
+    let availableCards = this.cards.filter((card) => card.checked);
+    if (availableCards.length > 1 && preventRepeat) {
+      availableCards = availableCards.filter((card) => card !== this.card);
+    }
+    if (availableCards.length < 1) {
+      const firstUncheckedOption = this.cardsOrSets.find(
+        (cardOrSet) => !cardOrSet.checked
+      );
+      firstUncheckedOption.checked = true;
+      return this.randomCard(preventRepeat);
+    }
     return availableCards[Math.floor(Math.random() * availableCards.length)];
   }
 }
 
-class Card {
+class Option {
+  get checkbox() {
+    return document.getElementById(this.id);
+  }
+
+  get checked() {
+    return (this._checked ||= localStorage.getItem(this.id) === "true");
+  }
+
+  set checked(value) {
+    this.setChecked(value, true);
+  }
+
+  appendOptionsTo(element, ...classes) {
+    const label = document.createElement("label");
+    label.htmlFor = this.id;
+    label.classList.add("option");
+    classes.forEach((className) => label.classList.add(className));
+
+    const input = document.createElement("input");
+    input.id = this.id;
+    input.type = "checkbox";
+    input.checked = this.checked;
+
+    const checkmark = document.createElement("div");
+    checkmark.className = "checkmark";
+
+    label.appendChild(input);
+    label.appendChild(checkmark);
+    label.append(this.name);
+    element.appendChild(label);
+
+    input.addEventListener("click", (event) => {
+      this.checked = event.target.checked;
+    });
+  }
+
+  setChecked(value, _cascade) {
+    this._checked = value;
+    localStorage.setItem(this.id, value);
+    if (this.checkbox) {
+      this.checkbox.checked = value;
+    }
+  }
+}
+
+class Set extends Option {
+  constructor(name, cards) {
+    super();
+    this.name = name;
+    this.id = `${getId(this)}-${getId(cards[0].constructor)}`;
+    this.cards = cards;
+    this.cards.forEach((card) => (card.set = this));
+  }
+
+  appendOptionsTo(element) {
+    super.appendOptionsTo(element, "set");
+    this.cards.forEach((card) => card.appendOptionsTo(element, "set-member"));
+  }
+
+  setChecked(value, cascade) {
+    super.setChecked(value, cascade);
+    if (cascade) {
+      this.cards.forEach((card) => card.setChecked(value, false));
+    }
+  }
+}
+
+class Card extends Option {
   constructor(name, { isLandscape = false, hasBack = false } = {}) {
+    super();
     const typeId = getId(this.constructor);
     this.name = name;
     this.id = getId(this);
@@ -105,6 +193,14 @@ class Card {
     this.backSrc = hasBack
       ? `images/${typeId}/${this.id}/back.png`
       : `images/${typeId}/back.png`;
+  }
+
+  setChecked(value, cascade) {
+    super.setChecked(value, cascade);
+    if (cascade && this.set) {
+      const allCardsInSetChecked = this.set.cards.every((card) => card.checked);
+      this.set.setChecked(allCardsInSetChecked, false);
+    }
   }
 }
 
@@ -130,31 +226,39 @@ class Aspect extends Card {
 
 const sections = [
   new Section([
-    new Scenario("Klaw"),
+    new Set("Core Set", [
+      new Scenario("Rhino"),
+      new Scenario("Klaw"),
+      new Scenario("Ultron"),
+    ]),
     new Scenario("Risky Business", { hasBack: true }),
-    new Scenario("Rhino"),
-    new Scenario("Ultron"),
   ]),
   new Section([
-    new Module("Bomb Scare", { isLandscape: true }),
+    new Set("Core Set", [
+      new Module("Bomb Scare", { isLandscape: true }),
+      new Module("Legions of Hydra", { isLandscape: true }),
+      new Module("Under Attack", { isLandscape: true }),
+      new Module("The Masters of Evil", { isLandscape: true }),
+      new Module("The Doomsday Chair", { isLandscape: true }),
+    ]),
     new Module("Goblin Gimmicks"),
-    new Module("Legions of Hydra", { isLandscape: true }),
-    new Module("The Doomsday Chair", { isLandscape: true }),
-    new Module("The Masters of Evil", { isLandscape: true }),
-    new Module("Under Attack", { isLandscape: true }),
   ]),
   new Section([
-    new Hero("Black Panther"),
-    new Hero("Captain Marvel"),
-    new Hero("Iron Man"),
-    new Hero("She-Hulk"),
-    new Hero("Spider-Man"),
+    new Set("Core Set", [
+      new Hero("Black Panther"),
+      new Hero("Captain Marvel"),
+      new Hero("Iron Man"),
+      new Hero("She-Hulk"),
+      new Hero("Spider-Man"),
+    ]),
   ]),
   new Section([
-    new Aspect("Aggression"),
-    new Aspect("Justice"),
-    new Aspect("Leadership"),
-    new Aspect("Protection"),
+    new Set("Core Set", [
+      new Aspect("Aggression"),
+      new Aspect("Justice"),
+      new Aspect("Leadership"),
+      new Aspect("Protection"),
+    ]),
   ]),
 ];
 
@@ -171,10 +275,10 @@ function setShuffleAllButtonAvailability() {
 
 // Initialisation steps.
 
+sections.forEach((section) => section.initialize());
+
 shuffleAllButton.addEventListener("click", () =>
   sections.forEach((section) => section.shuffle())
 );
-
-sections.forEach((section) => section.initialize());
 
 setTimeout(() => container.classList.remove("init"), 100);

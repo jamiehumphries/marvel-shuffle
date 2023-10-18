b943b0cca1f2aa66e81fdbf9f397d59b58344e0b
Jamie Humphries <jamie.humphries@softwire.com>
2023-10-18 16:03:09 +0100

    Bump version number.

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,4 +1,4 @@
-import { CardSet, Scenario, Module, Hero, Aspect } from "./options.js?v=2.2.2";
+import { CardSet, Scenario, Module, Hero, Aspect } from "./options.js?v=2.2.3";
 
 function ensureArray(arrayOrString) {
   return Array.isArray(arrayOrString) ? arrayOrString : [arrayOrString];

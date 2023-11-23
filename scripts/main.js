8fe62c12e37f216a6b05b81bb36a7f95becbd7ec
Jamie Humphries <jamie.humphries@softwire.com>
2023-11-23 14:59:50 +0000

    Bump version number.

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
-import { Setting, All } from "./options.js?v=2.3.4";
+import { Setting, All } from "./options.js?v=2.4.0";
 import {
   scenarios,
   modules,
   heroes,
   aspects,
   flatten,
-} from "./cards.js?v=2.3.4";
+} from "./cards.js?v=2.4.0";
 import {
   initializeStorage,
   clearStorage,
@@ -14,14 +14,14 @@ import {
   setUserId,
   getItem,
   setItem,
-} from "./storage.js?v=2.3.4";
+} from "./storage.js?v=2.4.0";
 import {
   clearTable,
   renderTable,
   initializeDifficultySettings,
   getTrackedDifficulties,
   isGameCompleted,
-} from "./tracker.js?v=2.3.4";
+} from "./tracker.js?v=2.4.0";
 
 const cardChangeDelayMs = Number(
   getComputedStyle(document.documentElement)
--- scripts/table.js
+++ scripts/table.js
@@ -1,6 +1,6 @@
-import { initializeStorage } from "./storage.js?v=2.3.4";
-import { scenarios, heroes } from "./cards.js?v=2.3.4";
-import { renderTable } from "./tracker.js?v=2.3.4";
+import { initializeStorage } from "./storage.js?v=2.4.0";
+import { scenarios, heroes } from "./cards.js?v=2.4.0";
+import { renderTable } from "./tracker.js?v=2.4.0";
 
 await initializeStorage();
 renderTable(scenarios, heroes);

0cfb545a49deb348810462f7e54dc8de3fbf9f87
Jamie Humphries <jamie.humphries@outlook.com>
2024-12-09 22:14:23 +0000

    Fix round.sh for deeper nesting

--- round.sh
+++ round.sh
@@ -1,2 +1,2 @@
 # Run from specifc card image folder.
-find -type f -name "*.png" | xargs -I {} convert {} -matte ../../../mask.png -compose DstIn -composite {}
+find -type f -name "*.png" | xargs -I {} convert {} -matte ../../../../mask.png -compose DstIn -composite {}

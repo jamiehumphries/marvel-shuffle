d5a0b0e191779a2b79854ef086297c74615a1270
Jamie Humphries <jamie.humphries@outlook.com>
2025-03-16 11:32:45 +0000

    Update build scripts

--- assets.js
+++
@@ -1,4 +0,0 @@
-import { updateAssetVersions, updateImageHashes } from "./tools.js";
-
-updateAssetVersions();
-updateImageHashes();
--- build.js
+++ build.js
@@ -1,24 +1,4 @@
-import { execSync } from "child_process";
 import { updateAssetVersions, updateImageHashes } from "./tools.js";
-
-const level = process.argv[2]?.slice(2) || "patch";
-
-execSync("git stash --include-untracked --quiet");
-
-const version = execSync(`npm version ${level} --no-git-tag-version`)
-  .toString()
-  .trim();
-
-console.log(`Building ${version}`);
-console.log();
 
 updateImageHashes();
 updateAssetVersions();
-
-execSync("git add package.json package-lock.json");
-execSync(`git commit --message="Build ${version}"`);
-execSync(`git tag --force ${version}`);
-
-try {
-  execSync("git stash pop --index --quiet");
-} catch {}

2b37ec79ef2eeef3b5414af80c3424fa1a757c6b
Jamie Humphries <jamie.humphries@outlook.com>
2025-11-10 14:57:34 +0000

    Allow faster keyboard navigation through tracker table

--- docs/scripts/handlers/nav.js
+++ docs/scripts/handlers/nav.js
@@ -34,17 +34,42 @@ function moveCell(event, dx, dy) {
     return;
   }
 
-  const td = input.closest("td");
-  const tr = input.closest("tr");
-  const tbody = input.closest("tbody");
-  const x = [...tr.children].indexOf(td);
-  const y = [...tbody.children].indexOf(tr);
+  event.preventDefault();
 
-  const next = tbody.children[y + dy]?.children[x + dx]?.querySelector("input");
-  if (!next) {
+  const stopOnBreak = event.ctrlKey ? (dx === 0 ? "row" : "col") : null;
+  const next = getNextInput(input, dx, dy, stopOnBreak);
+  if (next === input) {
     return;
   }
 
-  event.preventDefault();
+  const td = next.closest("td");
+  if (getComputedStyle(td).visibility === "hidden") {
+    td.classList.add("visible");
+  }
+
   next.focus();
+}
+
+function getNextInput(input, dx, dy, stopOnBreak) {
+  const td = input.closest("td");
+  const tr = input.closest("tr");
+  const tbody = input.closest("tbody");
+
+  const maxX = tr.children.length - 1;
+  const maxY = tbody.children.length - 1;
+
+  let x = [...tr.children].indexOf(td);
+  let y = [...tbody.children].indexOf(tr);
+
+  let next = input;
+  do {
+    x += dx;
+    y += dy;
+    if (x < 0 || x > maxX || y < 0 || y > maxY) {
+      break;
+    }
+    next = tbody.children[y].children[x].querySelector("input");
+  } while (stopOnBreak && !next.closest(`.${stopOnBreak}-break`));
+
+  return next;
 }
